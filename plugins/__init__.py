161e4afa298a5f6c8ea06228b1a210da482efba0
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2016-05-31 21:35:58 +0200

    Port to new module format

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,5 +1,5 @@
 """
-    Copyright 2015 Impera
+    Copyright 2016 Inmanta
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 
-    Contact: bart@impera.io
+    Contact: code@inmanta.com
 """
 
 import os
@@ -24,8 +24,8 @@ import tempfile
 import urllib
 import time
 
-from impera.agent.handler import provider, ResourceHandler
-from impera.plugins import plugin
+from inmanta.agent.handler import provider, ResourceHandler
+from inmanta.plugins import plugin
 
 from keystoneclient.auth.identity import v2
 from keystoneclient import session
