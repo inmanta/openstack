9aa17ec67c211bf0656837ba43c1fb9aa971b90a
Yaron Vanhulst <kazaamjt@gmail.com>
2019-10-23 16:02:01 +0200

    Update test

--- tests/test_flavor.py
+++ tests/test_flavor.py
@@ -1,16 +1,16 @@
 import inmanta
-import pytest
 
 def test_flavor(project):
     project.compile("""
-import unittest
 import openstack
-p = openstack::Provider(name="test", connection_url=std::get_env("OS_AUTH_URL"), username=std::get_env("OS_USERNAME"),
-                        password=std::get_env("OS_PASSWORD"), tenant=tenant)
+
+provider = openstack::Provider(name="test", connection_url=std::get_env("test"), username=std::get_env("test"),
+                        password=std::get_env("test"), tenant="test")
 flavor=openstack::Flavor(
-    name="test"
-    ram=1024
-    vcpus=4
+    provider=provider,
+    name="test",
+    ram=1024,
+    vcpus=4,
     disk=10
 )
-    """)
+""")
