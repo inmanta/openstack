a1d0ce6e8067a2c5050ba057c28c18a4f75d313c
Yaron Vanhulst <kazaamjt@gmail.com>
2019-10-29 14:24:13 +0100

    Update test to reflected changed state

--- tests/test_flavor.py
+++ tests/test_flavor.py
@@ -132,7 +132,7 @@ flavor=openstack::Flavor(
     assert updated_flavor.ram == 2048
 
     ctx_deploy_4 = project.deploy(updated_flavor)
-    assert ctx_deploy_4.status == inmanta.const.ResourceState.skipped
+    assert ctx_deploy_4.status == inmanta.const.ResourceState.failed
 
     ctx_dryrun_4 = project.dryrun(created_flavor)
     assert not ctx_dryrun_4.changes
