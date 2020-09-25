2c18a5cc4c5e5aae26598e7df4a4710229adfeec
Sander Van Balen <git@sandervanbalen.be>
2020-09-25 11:44:20 +0200

    added testcase

--- tests/test_nova.py
+++ tests/test_nova.py
@@ -15,9 +15,68 @@
 
     Contact: code@inmanta.com
 """
+import pytest
 import time
+from typing import Optional, Tuple
 
 import inmanta
+from inmanta.ast import ExplicitPluginException
+
+
+def get_model(
+    name: str,
+    key: str,
+    purged: Optional[bool] = False,
+    flavor_constraints: Optional[Tuple[int, int]] = None,
+) -> str:
+    if flavor_constraints is None:
+        flavor_constraints = (1, 0.5)
+
+    return """
+import unittest
+import openstack
+import ssh
+
+os = std::OS(name="cirros", version=0.4, family=std::linux)
+
+tenant = std::get_env("OS_PROJECT_NAME")
+p = openstack::Provider(
+    name="test",
+    connection_url=std::get_env("OS_AUTH_URL"),
+    username=std::get_env("OS_USERNAME"),
+    password=std::get_env("OS_PASSWORD"),
+    tenant=tenant,
+)
+key = ssh::Key(name="%(name)s", public_key="%(key)s")
+project = openstack::Project(provider=p, name=tenant, description="", enabled=true, managed=false)
+net = openstack::Network(provider=p, project=project, name="%(name)s", purged=%(purged)s)
+subnet = openstack::Subnet(
+    provider=p,
+    project=project,
+    network=net,
+    dhcp=true,
+    name="%(name)s",
+    network_address="10.255.255.0/24",
+    purged=%(purged)s,
+)
+vm = openstack::Host(
+    provider=p,
+    project=project,
+    key_pair=key,
+    name="%(name)s",
+    os=os,
+    image=openstack::find_image(p, os),
+    flavor=openstack::find_flavor(p, %(flavor_constraints)s),
+    user_data="",
+    subnet=subnet,
+    purged=%(purged)s,
+)
+    """ % {
+        "name": name,
+        "key": key,
+        "purged": str(purged).lower(),
+        "flavor_constraints": ", ".join(map(str, flavor_constraints)),
+    }
 
 
 def test_boot_vm(project, keystone, nova, neutron):
@@ -31,27 +90,7 @@ def test_boot_vm(project, keystone, nova, neutron):
     project.add_fact(
         "openstack::Host[dnetcloud,name=%s]" % name, "ip_address", "10.1.1.1"
     )
-    project.compile(
-        """
-import unittest
-import openstack
-import ssh
-
-os = std::OS(name="cirros", version=0.4, family=std::linux)
-
-tenant = std::get_env("OS_PROJECT_NAME")
-p = openstack::Provider(name="test", connection_url=std::get_env("OS_AUTH_URL"), username=std::get_env("OS_USERNAME"),
-                        password=std::get_env("OS_PASSWORD"), tenant=tenant)
-key = ssh::Key(name="%(name)s", public_key="%(key)s")
-project = openstack::Project(provider=p, name=tenant, description="", enabled=true, managed=false)
-net = openstack::Network(provider=p, project=project, name="%(name)s")
-subnet = openstack::Subnet(provider=p, project=project, network=net, dhcp=true, name="%(name)s",
-                           network_address="10.255.255.0/24")
-vm = openstack::Host(provider=p, project=project, key_pair=key, name="%(name)s", os=os,
-                     image=openstack::find_image(p, os), flavor=openstack::find_flavor(p, 1, 0.5), user_data="", subnet=subnet)
-        """
-        % {"name": name, "key": key}
-    )
+    project.compile(get_model(name, key))
 
     n1 = project.get_resource("openstack::Network", name=name)
     ctx = project.deploy(n1)
@@ -69,28 +108,7 @@ vm = openstack::Host(provider=p, project=project, key_pair=key, name="%(name)s",
     assert server is not None
 
     # cleanup
-    project.compile(
-        """
-import unittest
-import openstack
-import ssh
-
-os = std::OS(name="cirros", version=0.4, family=std::linux)
-
-tenant = std::get_env("OS_PROJECT_NAME")
-p = openstack::Provider(name="test", connection_url=std::get_env("OS_AUTH_URL"), username=std::get_env("OS_USERNAME"),
-                        password=std::get_env("OS_PASSWORD"), tenant=tenant)
-key = ssh::Key(name="%(name)s", public_key="%(key)s")
-
-project = openstack::Project(provider=p, name=tenant, description="", enabled=true, managed=false)
-net = openstack::Network(provider=p, project=project, name="%(name)s", purged=true)
-subnet = openstack::Subnet(provider=p, project=project, network=net, dhcp=true, name="%(name)s",
-                           network_address="10.255.255.0/24", purged=true)
-vm = openstack::Host(provider=p, project=project, key_pair=key, name="%(name)s", os=os, purged=true,
-                     image=openstack::find_image(p, os), flavor=openstack::find_flavor(p, 1, 0.5), user_data="", subnet=subnet)
-        """
-        % {"name": name, "key": key}
-    )
+    project.compile(get_model(name, key, purged=True))
 
     h1 = project.get_resource("openstack::VirtualMachine", name=name)
     ctx = project.deploy(h1)
@@ -147,3 +165,19 @@ vm = openstack::Host(provider=p, project=project, key_pair=key, name="%(name)s",
     if len(networks) > 0:
         for network in networks:
             neutron.delete_network(network["id"])
+
+
+def test_59_find_flavor_exception(project, keystone, nova) -> None:
+    name = "inmanta-unit-test"
+    key = (
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCsiYV4Cr2lD56bkVabAs2i0WyGSjJbuNHP6IDf8Ru3Pg7DJkz0JaBmETHNjIs+yQ98DNkwH9gZX0"
+        "gfrSgX0YfA/PwTatdPf44dwuwWy+cjS2FAqGKdLzNVwLfO5gf74nit4NwATyzakoojHn7YVGnd9ScWfwFNd5jQ6kcLZDq/1w== "
+        "bart@wolf.inmanta.com"
+    )
+    cpus: int = 4096
+    ram: int = 4096
+    with pytest.raises(
+        ExplicitPluginException,
+        match=f"Couldn't find a flavor with at least {cpus} unpinned CPUs and {ram} Gigabytes of RAM",
+    ):
+        project.compile(get_model(name, key), flavor_constraints=(cpus, ram))
